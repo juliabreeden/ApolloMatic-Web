f2643b52a7d89d773a7fce8d23e4bcbab38284e8
Davis Kim <dkim0509@gmail.com>
2023-11-06 22:11:23 -0500

    deleted extra files

---
+++ apollomatic-web-app/src/components/GraphqlResolvers.jsx
@@ -0,0 +1,10 @@
+const GraphqlResolvers = () => {
+    return (
+        <div>
+            <label for="GraphQLSchema">GraphQL Schema</label>
+            <div className = "graphqlresolvers-container"></div>
+        </div>
+    );
+};
+
+export default GraphqlResolvers; 
---
+++ apollomatic-web-app/src/components/MongoSchema.jsx
@@ -0,0 +1,10 @@
+const MongoSchema = () => {
+    return (
+        <div>
+        <label for="MongoSchema">Mongo Schema</label>
+        <input type="text" id="MongoSchema" placeholder ="Input Mongo Schema here."></input>
+        </div>
+    )
+};
+
+export default MongoSchema; 
