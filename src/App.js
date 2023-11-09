499f0716255a069cca13aff76cd4f69df03c7e47
Davis Kim <dkim0509@gmail.com>
2023-11-08 21:43:26 -0500

    new change

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Playground from "./components/Playground.jsx";
+import ApolloNavbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import "./style/App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    // <div className="App">
+    //  ApolloMatic
+    // </div>
+    <Router>
+      <ApolloNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/playground" element={<Playground />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
